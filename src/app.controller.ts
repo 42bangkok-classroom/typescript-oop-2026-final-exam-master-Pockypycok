48be21602a12fed2dd4e30f605538299b3b66a51
pockypycok <cheophy8@gmail.com>
2026-03-25 11:58:31 +0700

    llll

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiResponse } from './interfaces/response.interface';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+  getHello(): ApiResponse <{
+    service : "string"
+    version : "string"
+
+    return 
+    success: true,
+    data : this.appService.getHello();
+    message : 'Hello NestJS'
+  >}
 }
